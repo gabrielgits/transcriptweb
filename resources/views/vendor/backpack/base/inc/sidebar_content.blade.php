4c09b28358badddf68dc720d386d25f9b4185c34
gabrielgits <gvgabrielvieiragabrielvieira@gmail.com>
2025-01-17 15:30:41 +0100

    block other user

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,5 +1,7 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
+
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+@if(backpack_user()->id == 1)
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('user') }}'><i class='nav-icon la la-question'></i> Users</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('course') }}'><i class='nav-icon la la-question'></i> Courses</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('student') }}'><i class='nav-icon la la-question'></i> Students</a></li>
@@ -11,3 +13,4 @@
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('test') }}'><i class='nav-icon la la-question'></i> Tests</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('students-answer') }}'><i class='nav-icon la la-question'></i> Students answers</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('dailypoint') }}'><i class='nav-icon la la-question'></i> Dailypoints</a></li>
+@endif
